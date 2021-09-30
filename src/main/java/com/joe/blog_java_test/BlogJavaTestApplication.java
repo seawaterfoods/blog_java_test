30a0660e55ac4dbbf006eb0a3f62f199e10a079b
Joe.wu <joe.wu@shoalter.com>
2021-09-30 14:46:24 +0800

    Initial commit

---
+++ src/main/java/com/joe/blog_java_test/BlogJavaTestApplication.java
@@ -0,0 +1,15 @@
+package com.joe.blog_java_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJavaTestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BlogJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joe/blog_java_test/BlogJavaTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.joe.blog_java_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJavaTestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
